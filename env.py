2f561fdf7632694a23466a9b5e47173a35b468ff
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-08-02 00:36:00 +0100

    First commit, App setup

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5100")
+os.environ.setdefault("SECRET_KEY", "secret_key_1")
+os.environ.setdefault("DEBUG", "TRUE")
+os.environ.setdefault("DEVELOPMENT", "TRUE")
+os.environ.setdefault("DB_URL", "postgres://postgres:damsey/taskmanager")
