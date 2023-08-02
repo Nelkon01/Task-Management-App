2c2aa5dd6c6d05ce9a8271aa59b14dbac55c397c
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-08-02 09:15:09 +0100

    Add database schema and configurations

---
+++ taskmanager/models.py
@@ -0,0 +1,26 @@
+from taskmanager import db
+
+
+class Category(db.Model):
+    # schema for the category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(20), unique=True, nullable=True)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ method to return a string representation of the model
+        return self
+
+
+class Task(db.Model):
+    # schema for the task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # represent itself in the form of a string
+        return f"#{self.id} - Task{self.task_name} | Urgent: {self.is_urgent}"
